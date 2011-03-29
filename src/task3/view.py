32f38a4126c35b56b29e734f52e19b1068222b34
aifreedom <me@aifreedom.com>
2011-03-29 12:05:18 +0800

    Add treeview.

--- src/task3/view.py
+++ src/task3/view.py
@@ -7,8 +7,23 @@ class Monitor (View):
     top = "main_window"
 
     def set_cpu_info(self, msg):
-        print msg
         self['label5'].set_text(msg)
         return
     
+    def set_proc_list_info(self, new):
+        tv = self['treeview']
+        tv.set_model(new)
+
+        rend = gtk.CellRendererText()
+        col = gtk.TreeViewColumn('Pid', rend, text=0)
+        col.set_name('col_pid')
+        tv.append_column(col)
+
+        rend = gtk.CellRendererText()
+        col = gtk.TreeViewColumn('Process Name', rend, text=1)
+        col.set_name('col_proc')
+        tv.append_column(col)
+        
+        pass
+        
     pass
